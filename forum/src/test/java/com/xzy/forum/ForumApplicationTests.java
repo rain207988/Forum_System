a2c2a26923b23e47ad2fc0347c3f95c44342f7e6
rain207988 <rain207988@gamil.com>
2026-03-01 12:42:56 +0800

    首次提交

---
+++ forum/src/test/java/com/xzy/forum/ForumApplicationTests.java
@@ -0,0 +1,14 @@
+package com.xzy.forum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+        System.out.println("Hello, ForumApplicationTests!");
+	}
+
+}
